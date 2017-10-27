ab1ef03ad97427ea889afa178994028c518c6c30
rizaldi <rizaldi.95@gmail.com>
2017-10-27 15:43:55 +0700

    first commit for a new web app

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+
+@section('content')
+    <h1>about</h1>
+    <p>this is about page</p>
+@endsection
+
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+
+@section('content')
+    <div class="jumbotron text-center">
+        <h1>Welcome To My Website</h1>
+        <p>silakan lihat lihat sesuka hati</p>
+    </div>
+@endsection
+
